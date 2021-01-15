d93930cb588b643231d6866135800cae55521f49
DrogHead <giuvit003@gmail.com>
2021-01-15 11:51:56 +0100

    sendhelppls

---
+++ dialog/button/button.ts
@@ -0,0 +1,69 @@
+import Ripple from './ripple/ripple.js';
+
+export default class Button {
+
+    link: HTMLLinkElement;
+    ripple: Ripple;
+    initialize: () => void;
+    destroy: () => void;
+
+    initialized: boolean;
+
+    constructor(path: string) {
+
+        let ripple = new Ripple(path + 'ripple/');
+        this.ripple = ripple;
+
+        let link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = path + 'button.css';
+        this.link = link;
+
+        this.initialized = false;
+
+        this.initialize = () => {
+
+            this.ripple.init();
+
+            document.head.appendChild(this.link);
+
+        };
+        this.destroy = () => {
+
+            this.ripple.end();
+
+            try {
+
+                document.head.removeChild(this.link);
+
+            } catch (e) {
+
+            }
+
+        };
+
+    }
+
+    init() {
+
+        if (!this.initialized) {
+
+            this.initialize();
+            this.initialized = true;
+
+        }
+
+    }
+
+    end() {
+
+        if (this.initialized) {
+
+            this.destroy();
+            this.initialized = false;
+
+        }
+
+    }
+
+}
